40b15561ab076b57ee6acd38dbdf3a9a4678c63c
River Wijaya <riverwijaya9@gmail.com>
2026-03-20 11:37:10 +0700

    Revert "switch to astro"
    This reverts commit 7d366b7a22483116aa6846fca82f84f2031bfba9.

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://onchch.github.io',
-  // Change the line above to your custom domain once you have one,
-  // e.g. 'https://riverwijaya.com'
-});
