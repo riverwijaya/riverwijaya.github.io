315c67e46c0148e2fc05adb07b1769c649446acc
River Wijaya <riverwijaya9@gmail.com>
2026-03-29 22:06:40 +0700

    pls

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://onchch.github.io',
+  site: 'https://riverwijaya.github.io',
   // Change to your custom domain once set up, e.g.:
   // site: 'https://riverwijaya.com'
 });
